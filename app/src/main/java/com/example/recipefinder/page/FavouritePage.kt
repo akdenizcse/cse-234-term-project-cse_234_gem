f3995143171e9f30bf8a9551ad7c0fc56f8dfc9b
Goktug Berke <gungorengoktugberke@gmail.com>
2024-06-09 14:26:36 +0300

    Göktuğ - Favorites Page created and fixed

--- app/src/main/java/com/example/recipefinder/page/FavouritePage.kt
+++ app/src/main/java/com/example/recipefinder/page/FavouritePage.kt
@@ -3,6 +3,7 @@ package com.example.recipefinder
 import android.util.Log
 import androidx.compose.material.MaterialTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,11 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,8 +29,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +51,10 @@ fun FavouritePage(navController: NavController) {
     var favoriteMeals by remember { mutableStateOf<List<Meal>>(emptyList()) }
     val user = FirebaseAuth.getInstance().currentUser
     val db = FirebaseFirestore.getInstance()
+    val orangeColor = Color(0xFFFF8C00)
+    val orangeGradient = Brush.verticalGradient(
+        colors = listOf(orangeColor, orangeColor.copy(alpha = 0.7f))
+    )
     val userId = user?.uid
     if (userId == null) {
         // Handle the case where user is not logged in
@@ -81,9 +90,39 @@ fun FavouritePage(navController: NavController) {
         }
     }
 
-    LazyColumn {
-        items(favoriteMeals) { meal ->
-            RecipeItem(navController = navController, meal = meal, modifier = Modifier)
+    Column(modifier = Modifier.padding(16.dp)
+        .fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(orangeGradient)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                contentDescription = "Back",
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable { navController.popBackStack() },
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Favorites",
+                style = androidx.compose.material3.MaterialTheme.typography.headlineSmall.copy(fontSize = 24.sp),
+                color = Color.White
+
+
+            )
+            }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(favoriteMeals) { meal ->
+                RecipeItem(navController = navController, meal = meal, modifier = Modifier)
+                Spacer(modifier = Modifier.height(8.dp)) // Itemlar arasına boşluk ekliyoruz
+            }
         }
     }
 }
@@ -121,14 +160,14 @@ fun RecipeItem(navController: NavController, meal: Meal, modifier: Modifier) {
                     fontWeight = FontWeight.Bold,
                     fontSize = 18.sp
                 )
-                Spacer(modifier = Modifier.height(4.dp))
+                Spacer(modifier = Modifier.height(1.5.dp))
                 // Example rating row
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Spacer(modifier = Modifier.width(4.dp))
                     Text(
-                        text = "$ ★",
+                        text = "4.5 ★",
                         color = Color.Gray
                     )
                 }
